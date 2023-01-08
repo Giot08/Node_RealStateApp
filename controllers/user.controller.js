bd7d754d60e682cd3fae6bb8bcee1a1df011cabb
giot08 <xgiot08@gmail.com>
2023-01-08 01:14:01 -0300

    sesion 6 ok

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,22 @@
 const formLogin = (req,res) => {
     res.render('auth/login', {
+        pagina: 'Login'
     })
 }
 const formRegister = (req,res) => {
     res.render('auth/register', {
+        pagina: 'Register'
+    })
+}
+
+const formForgotPassword = (req,res) => {
+    res.render('auth/forgot_password', {
+        pagina: 'Forgot password'
     })
 }
 
 export {
     formLogin,
-    formRegister
+    formRegister,
+    formForgotPassword
 }
--- routes/auth/login.routes.js
+++ routes/auth/login.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {formLogin, formRegister} from "../../controllers/user.controller.js"
+import {formLogin, formRegister,formForgotPassword} from "../../controllers/user.controller.js"
 
 const router = express.Router()
 
 // routing
 router.get('/login', formLogin);
 router.get('/register', formRegister);
+router.get('/forgot_password', formForgotPassword);
 
 export default router;
